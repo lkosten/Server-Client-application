535baf5270b0def96e5973324b08fe7c68608bd7
Kostya Yarmosh <kyarmash@gmail.com>
2020-06-05 21:55:26 +0300

    add commands reading

--- Client/Client/Client.cpp
+++ Client/Client/Client.cpp
@@ -1,1 +1,31 @@
 #include "Client.h"
+
+
+void Client::readData()
+{
+  std::wifstream inputFile;
+  inputFile.open(commandsFileName);
+  if (!inputFile)
+  {
+    std::cerr << "Can't open file with commands!" << std::endl;
+    exit(0);
+  }
+
+  while (!inputFile.eof())
+  {
+    std::wstring command;
+    inputFile >> command;
+    commands.push_back(command);
+  }
+
+  inputFile.close();
+}
+
+Client::Client(size_t requestNumber) : requestNumber(requestNumber)
+{
+
+}
+
+Client::~Client()
+{
+}
--- Client/Client/Client.h
+++ Client/Client/Client.h
@@ -1,6 +1,19 @@
 #pragma once
+#include"Headers.h"
 
 class Client
 {
+  const std::string commandsFileName = "commands.xml";
+  size_t requestNumber;
+
+  std::vector<std::wstring> 
+
+  void readData();
+
+public:
+
+  Client(size_t requestNumber);
+  ~Client();
+
 };
 
