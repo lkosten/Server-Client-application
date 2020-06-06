f49d99ceef29bab1fef9cdf1f226a837523253f9
Kostya Yarmosh <kyarmash@gmail.com>
2020-06-06 17:11:37 +0300

    add tuneNetwork() function

--- Client/Client/Client.cpp
+++ Client/Client/Client.cpp
@@ -21,6 +21,37 @@ void Client::readData()
   inputFile.close();
 }
 
+void Client::tuneNetwork()
+{
+  WSAData wsaData;
+  if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+  {
+    std::cerr << "Can't initialize winsock!" << std::endl;
+    exit(0);
+  }
+
+  clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+  if (clientSocket == INVALID_SOCKET)
+  {
+    std::cerr << "Initializing socket failed with error: " << WSAGetLastError() << std::endl;
+    WSACleanup();
+    exit(0);
+  }
+
+  sockaddr_in hint;
+  hint.sin_family = AF_INET;
+  hint.sin_port = htons(port);
+  inet_pton(AF_INET, serverIpAddress.c_str(), &hint);
+
+  if (connect(clientSocket, (sockaddr*)&hint, sizeof(hint)) == SOCKET_ERROR)
+  {
+    std::cerr << "Failed to connect to server with error: " << WSAGetLastError() << std::endl;
+    closesocket(clientSocket);
+    WSACleanup();
+    exit(0);
+  }
+}
+
 Client::Client(size_t requestNumber) : requestNumber(requestNumber)
 {
 
@@ -28,4 +59,6 @@ Client::Client(size_t requestNumber) : requestNumber(requestNumber)
 
 Client::~Client()
 {
+  closesocket(clientSocket);
+  WSACleanup();
 }
--- Client/Client/Client.h
+++ Client/Client/Client.h
@@ -4,11 +4,17 @@
 class Client
 {
   const std::string commandsFileName = "commands.xml";
+  const std::string serverIpAddress = "127.0.0.1";
+  const uint32_t port = 8731;
+
   size_t requestNumber;
 
-  std::vector<std::wstring> 
+  SOCKET clientSocket;
+
+  std::vector<std::wstring> commands;
 
   void readData();
+  void tuneNetwork();
 
 public:
 
